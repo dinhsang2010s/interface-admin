d971362ad7fcf56d07c72c59922b50e64e605305
dinhsang2010s <nguyendinhsang.it98@gmail.com>
2023-12-05 01:54:11 +0700

    add-css

--- src/components/PageInit/index.tsx
+++ src/components/PageInit/index.tsx
@@ -1,3 +1,4 @@
+import "./style.less";
 import { Button } from "antd";
 import Search from "../Search";
 
@@ -13,48 +14,25 @@ const PageInit = (props: Props) => {
   };
 
   return (
-    <div
-      className={`page ${props.title.toLowerCase()}`}
-      style={{
-        background: "var(--bg-component)",
-        borderRadius: "6px",
-      }}
-    >
-      <div
-        className="page-nav flex"
-        style={{
-          justifyContent: "space-between",
-        }}
-      >
-        <div
-          className="flex-center"
-          style={{
-            background: "var(--bg-item-category-selected)",
-            padding: "0px 32px",
-            width: "25%",
-            color: "var(--text-item-selected)",
-            borderTopLeftRadius: 6,
-          }}
-        >
-          <h3 className="nav-title">{`${props.title} lists`}</h3>
-        </div>
-        <div style={{ paddingTop: 20, paddingRight: 20 }}>
-          <div className="nav-function flex">
-            <Search
-              placeholder={`Search ${props.title}...`}
-              onChange={onChange}
-            />
-            <Button
-              style={{ marginLeft: 20 }}
-              type="primary"
-              icon={<i className="fa-solid fa-plus"></i>}
-            >
-              {`Add ${props.title}`}
-            </Button>
-          </div>
+    <div className={`page ${props.title.toLowerCase()}`}>
+      <div className="page-nav flex">
+        <h3 className="nav-title">{`${props.title} lists`}</h3>
+        <div className="nav-function flex">
+          <Search
+            placeholder={`Search ${props.title}...`}
+            onChange={onChange}
+            style={{ maxWidth: 350 }}
+          />
+          <Button
+            style={{ marginLeft: 20 }}
+            type="primary"
+            icon={<i className="fa-solid fa-plus"></i>}
+          >
+            {`Add ${props.title}`}
+          </Button>
         </div>
       </div>
-      <div className="page-children" style={{ padding: "15px 20px 0px" }}>
+      <div className="page-children" style={{ padding: "10px 20px" }}>
         {props.children}
       </div>
     </div>
