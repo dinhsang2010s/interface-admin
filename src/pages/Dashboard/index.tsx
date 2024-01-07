08bcb762f77d2fa4e16e2b315f529c19e54b97a9
dinhsang2010s <nguyendinhsang.it98@gmail.com>
2024-01-08 02:19:09 +0700

    factory-project

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,25 +1,5 @@
-import React, { useState } from "react";
-import { Carousel, Col, Row, Tabs } from "antd";
 import "./style.less";
 
-const items = [
-  {
-    key: "1",
-    label: "Tab 1",
-    children: "Content of Tab Pane 1",
-  },
-  {
-    key: "2",
-    label: "Tab 2",
-    children: "Content of Tab Pane 2",
-  },
-  {
-    key: "3",
-    label: "Tab 3",
-    children: "Content of Tab Pane 3",
-  },
-];
-
 export const Dashboard = () => {
-  return <div className="dashboard">dashboard</div>;
+    return <div className="dashboard">dashboard</div>;
 };
